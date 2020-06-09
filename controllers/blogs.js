0e4c2a24659855013278cc85b448590f3343a4ed
Shubham Saurav <shubhamsinha186@gmail.com>
2020-06-09 21:20:31 +0530

    Handled All Errors With Error Handler |  Untested

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,91 +1,70 @@
 const Blog = require('../models/Blog');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 // @route       : GET /api/v1/blogs
 // @route       : GET /api/v1/categories/:categoryId/blogs
 // @desc        : Get all blogs
 // @access      : Public
-exports.getBlogs = async (req, res, next) => {
-  try {
-    console.log(req.params.categoryId);
-    if (req.params.categoryId) {
-      const blogs = await Blog.find({ category: req.params.categoryId });
-      res.status(200).json({ success: true, count: blogs.length, data: blogs });
-    } else {
-      const blogs = await Blog.find();
-      res.status(200).json({ success: true, count: blogs.length, data: blogs });
-    }
-  } catch (error) {
-    res.status(400).json({ success: false, message: 'Something went wrong!' });
+exports.getBlogs = catchAsync(async (req, res, next) => {
+  // console.log(req.params.categoryId);
+  if (req.params.categoryId) {
+    const blogs = await Blog.find({ category: req.params.categoryId });
+    res.status(200).json({ success: true, count: blogs.length, data: blogs });
+  } else {
+    const blogs = await Blog.find();
+    res.status(200).json({ success: true, count: blogs.length, data: blogs });
   }
-};
+});
 
 // @route       : GET /api/v1/blogs/:id
 // @desc        : Get a blog
 // @access      : Public
-exports.getBlog = async (req, res, next) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-    if (!blog) {
-      return res.status(404).json({
-        succes: false,
-        message: `Blog not found with the id of ${req.params.id}`,
-      });
-    }
-    res.status(200).json({ success: true, data: blog });
-  } catch (error) {
-    res.status(400).json({ success: false, message: 'Something went wrong!' });
+exports.getBlog = catchAsync(async (req, res, next) => {
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    return next(
+      new AppError(`Blog not found with the Id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: blog });
+});
 
 // @route       : POST /api/v1/categories/:categoryId/blogs
 // @desc        : Create a blog
 // @access      : Private
-exports.createBlog = async (req, res, next) => {
-  try {
-    req.body.category = req.params.categoryId;
-    const blog = await Blog.create(req.body);
-    res.status(200).json({ success: true, data: blog });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ success: false, message: 'Something went wrong!' });
-  }
-};
+exports.createBlog = catchAsync(async (req, res, next) => {
+  req.body.category = req.params.categoryId;
+  const blog = await Blog.create(req.body);
+  res.status(200).json({ success: true, data: blog });
+});
 
 // @route       : PUT /api/v1/blogs/:id
 // @desc        : Update a Blog
 // @access      : Private
-exports.updateBlog = async (req, res, next) => {
-  try {
-    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!blog) {
-      return res.status(404).json({
-        succes: false,
-        message: `Blog not found with the id of ${req.params.id}`,
-      });
-    }
-    res.status(200).json({ success: true, data: blog });
-  } catch (error) {
-    res.status(400).json({ success: false, message: 'Something went wrong!' });
+exports.updateBlog = catchAsync(async (req, res, next) => {
+  const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!blog) {
+    return next(
+      new AppError(`Blog not found with the Id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: blog });
+});
 
 // @route       : DELETE /api/v1/blogs/:id
 // @desc        : Delete a Blog
 // @access      : Private
-exports.deleteBlog = async (req, res, next) => {
-  try {
-    const blog = await Blog.findByIdAndDelete(req.params.id);
-    if (!blog) {
-      return res.status(404).json({
-        succes: false,
-        message: `Blog not found with the id of ${req.params.id}`,
-      });
-    }
-    res.status(200).json({ success: true, data: {} });
-  } catch (error) {
-    res.status(400).json({ success: false, message: 'Something went wrong!' });
+exports.deleteBlog = catchAsync(async (req, res, next) => {
+  const blog = await Blog.findByIdAndDelete(req.params.id);
+  if (!blog) {
+    return next(
+      new AppError(`Blog not found with the Id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ success: true, data: {} });
+});
